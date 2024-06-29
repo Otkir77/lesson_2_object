0cccc8c59813f9c46b24df299076ad716d58e550
Otkir77 <166150681+Otkir77@users.noreply.github.com>
2024-06-29 14:50:44 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let ism = prompt('Ismingizni kiriting')
+let spaceid = prompt('Loginingizni kiriting')
+let password = prompt('Parolingiz kiriting')
+
+let obj = {
+}
+
+obj.name = ism
+obj.login = spaceid
+obj.parol = password
+
+
+console.log(obj);
+
